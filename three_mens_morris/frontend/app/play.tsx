17470677a47a7bdfe8cf3afc4ebf1e57653136ff
minsung.cho <ms98.cho@gmail.com>
2024-06-19 17:27:51 -0700

    makes it work a bit better with bot

--- three_mens_morris/frontend/app/play.tsx
+++ three_mens_morris/frontend/app/play.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Modal } from "react-native";
 import { colorPalette } from "./colors";
+
 // import { Link } from "expo-router";
 
 import { Board } from "../components/Board";
@@ -8,33 +9,15 @@ import Bot from "../components/Bot";
 import { isValidMove, indexToRowCol } from "../components/Rules";
 
 export default function PlayPage() {
-  const bot = useRef(new Bot(1)).current;
+  const bot = new Bot;
 
   const [board, setBoard] = useState(Array.from({ length: 9 }, () => 0));
-  const [turn, setTurn] = useState(1);
-  const [winner, setWinner] = useState(0);
-  const [debug, setDebug] = useState("");
 
-  const [playerPiece, setPlayerPiece] = useState(1);
-  const [botPiece, setBotPiece] = useState(2);
   const [playerRemaining, setPlayerRemaining] = useState(3);
   const [botRemaining, setBotRemaining] = useState(3);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalText, setModalText] = useState("");
   const [pressedIndex, setPressedIndex] = useState(-1);
-  const turnColor = turn === 1 ? "black" : "white";
-  const [turnText, setTurnText] = useState(
-    turn === playerPiece ? "Player" : "Bot"
-  );
-
-  useEffect(() => {
-    setTurnText(turn === playerPiece ? "Player" : "Bot");
-  }, [turn]);
-  //   const turnText =
-  //     (turn === playerPiece && playerRemaining > 0) ||
-  //     (turn === botPiece && botRemaining > 0)
-  //       ? "Turn"
-  //       : "Move";
 
   const modalView = () => {
     return (
@@ -53,7 +36,6 @@ export default function PlayPage() {
             <TouchableOpacity
               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
               onPress={() => {
-                resetGame();
                 setModalVisible(!modalVisible);
               }}
             >
@@ -65,143 +47,106 @@ export default function PlayPage() {
     );
   };
 
-  const checkWin = (newBoard: number[], player: number) => {
-    for (let i = 0; i < 3; i++) {
+  const checkWin = (newBoard: number[]): boolean => {
+    for (let player = 1; player <= 2; player++) {
+      for (let i = 0; i < 3; i++) {
+        if (
+          (newBoard[i * 3] === player &&
+            newBoard[i * 3] === newBoard[i * 3 + 1] &&
+            newBoard[i * 3] === newBoard[i * 3 + 2]) ||
+          (newBoard[i] === player &&
+            newBoard[i] === newBoard[i + 3] &&
+            newBoard[i] === newBoard[i + 6])
+        ) {
+          if (player === 1) {
+            setModalText("Player won!");
+          }
+          if (player === 2) {
+            setModalText("Bot won!");
+          }
+          setModalVisible(true);
+          return true;
+        }
+      }
+
+      // Check diagonals
       if (
-        (newBoard[i * 3] === player &&
-          newBoard[i * 3] === newBoard[i * 3 + 1] &&
-          newBoard[i * 3] === newBoard[i * 3 + 2]) ||
-        (newBoard[i] === player &&
-          newBoard[i] === newBoard[i + 3] &&
-          newBoard[i] === newBoard[i + 6])
+        (newBoard[0] === player &&
+          newBoard[0] === newBoard[4] &&
+          newBoard[0] === newBoard[8]) ||
+        (newBoard[2] === player &&
+          newBoard[2] === newBoard[4] &&
+          newBoard[2] === newBoard[6])
       ) {
-        setWinner(player);
-        setModalText("Game over. Somebody won!");
+        if (player === 1) {
+          setModalText("Player won!");
+        }
+        if (player === 2) {
+          setModalText("Bot won!");
+        }
         setModalVisible(true);
+        return true;
       }
     }
-
-    // Check diagonals
-    if (
-      (newBoard[0] === player &&
-        newBoard[0] === newBoard[4] &&
-        newBoard[0] === newBoard[8]) ||
-      (newBoard[2] === player &&
-        newBoard[2] === newBoard[4] &&
-        newBoard[2] === newBoard[6])
-    ) {
-      setWinner(player);
-      setModalText("Game over. Somebody won!");
-      setModalVisible(true);
-    }
-  };
-
-  const updateBoard = (newIndex: number, oldIndex?: number) => {
-    // update that current turn value to the board.
-    // if old index is given then take the value of old index and assign it to new index.
-    const newBoard = [...board];
-
-    // new piece
-    if (oldIndex === undefined) {
-      // reject if turn player does not have remaining pieces to play.
-      if (turn === playerPiece && playerRemaining <= 0) {
-        return;
-      }
-      if (turn === botPiece && botRemaining <= 0) {
-        return;
-      }
-
-      // reject if new index is not empty
-      if (board[newIndex] != 0) {
-        return;
-      }
-
-      if (turn === playerPiece) {
-        setPlayerRemaining(playerRemaining - 1);
-        newBoard[newIndex] = playerPiece;
-      } else {
-        setBotRemaining(botRemaining - 1);
-        newBoard[newIndex] = botPiece;
-      }
-    } else {
-      // move piece
-      // reject if there's remaining pieces
-      if (turn === playerPiece && playerRemaining > 0) {
-        return;
-      }
-      if (turn === botPiece && botRemaining > 0) {
-        return;
-      }
-
-      // if num does not match old index then return.
-      if (turn === playerPiece && board[oldIndex] != playerPiece) {
-        return;
-      }
-      if (turn === botPiece && board[oldIndex] != botPiece) {
-        return;
-      }
-
-      // reject if new index is not zero
-      if (board[newIndex] != 0) {
-        return;
-      }
-      // reject if move is invalid
-      if (!isValidMove(oldIndex, newIndex)) {
-        return;
-      }
-      newBoard[oldIndex] = 0;
-      if (turn === playerPiece) {
-        newBoard[newIndex] = playerPiece;
-      } else {
-        newBoard[newIndex] = botPiece;
-      }
-    }
-
-    setPressedIndex(-1);
-    setBoard(newBoard);
-    checkWin(newBoard, turn);
+    return false;
   };
 
   const handlePress = (index: number) => {
     // this function has two goals:
     // 1. set pressed index on its own piece
     // 2. call updateBoard
-    // reject if it's bot's turn
-    console.log("what handle press thinks turn:", turn);
-    console.log("player turn:", playerPiece);
+    // 3. let bot play
 
-    if (turn !== playerPiece) {
-      return;
-    }
+    const newBoard = [...board];
 
     // new piece
-    if (board[index] === 0 && pressedIndex === -1) {
-      // reject if turn player does not have any remaining pieces but did not indicate to move existing pieces
-      updateBoard(index);
-      return;
+    if (newBoard[index] === 0 && pressedIndex === -1) {
+      // reject if player does not have any remaining pieces but did not indicate to move existing pieces
+      if (playerRemaining <= 0) {
+        setModalText("Invalid move!");
+        return setModalVisible(true);
+      }
+      newBoard[index] = 1;
+      let win = checkWin(newBoard);
+      if (win) {
+        return setBoard(newBoard);
+      }
+      console.log("after player:", newBoard);
+      if (botRemaining > 0) {
+        let newIndex = bot.play_new(newBoard, playerRemaining);
+        newBoard[newIndex] = 2;
+        console.log("bot board:", newBoard);
+      } else {
+        let [newIndex, oldIndex] = bot.play_move(newBoard);
+        newBoard[oldIndex] = 0;
+        newBoard[newIndex] = 2;
+        console.log("bot board:", newBoard);
+      }
+      setPlayerRemaining(playerRemaining - 1);
+      setBotRemaining(botRemaining - 1);
+      win = checkWin(newBoard);
+      return setBoard(newBoard);
     }
 
     // player clicks its own piece to move
     if (
-      board[index] != 0 &&
       pressedIndex === -1 &&
-      board[index] === playerPiece
+      board[index] === 1 && playerRemaining <= 0
     ) {
       setPressedIndex(index);
       return;
     }
 
     // cancelling selected piece
-    if (index === pressedIndex && board[index] === playerPiece) {
+    if (index === pressedIndex && board[index] === 1) {
       setPressedIndex(-1);
       return;
     }
 
     // Player is changing what piece to move
     if (
-      board[index] === playerPiece &&
-      pressedIndex != -1 &&
-      board[pressedIndex] === playerPiece
+      board[index] === board[pressedIndex] &&
+      pressedIndex != -1 && playerRemaining <= 0
     ) {
       setPressedIndex(index);
       return;
@@ -211,10 +156,28 @@ export default function PlayPage() {
     if (
       playerRemaining <= 0 &&
       pressedIndex != -1 &&
-      board[pressedIndex] === playerPiece
+      board[index] === 0
     ) {
-      updateBoard(index, pressedIndex);
-      return;
+      // validate move
+      if (!isValidMove(pressedIndex, index)) {
+        setModalText("Invalid move!");
+        return setModalVisible(true);
+      };
+      newBoard[pressedIndex] = 0;
+      newBoard[index] = 1;
+      console.log("after player:", newBoard);
+      setPressedIndex(-1);
+      let win = checkWin(newBoard);
+      if (win) {
+        return setBoard(newBoard);
+      }
+      // let bot play
+      let [oldIndex, newIndex] = bot.play_move(newBoard);
+      newBoard[oldIndex] = 0;
+      newBoard[newIndex] = 2;
+      console.log("after bot:", newBoard);
+      checkWin(newBoard);
+      return setBoard(newBoard);
     }
   };
 
@@ -223,72 +186,24 @@ export default function PlayPage() {
     setPlayerRemaining(3);
     setBotRemaining(3);
     setPressedIndex(-1);
-    setWinner(0);
-    setTurn(1);
-
-    // randomize player one and player two
-    if (Math.random() < 0.5) {
-      setPlayerPiece(1);
-      setBotPiece(2);
-      bot.changePiece(2);
-    } else {
-      setBotPiece(1);
-      setPlayerPiece(2);
-      bot.changePiece(1);
-    }
-
-    console.log("");
-    console.log("turn:", turn);
-    console.log("player piece:", playerPiece);
-    console.log("bot piece:", botPiece);
-    console.log("");
-  };
-
-  const letBotPlay = () => {
-    // let bot play
-    // reject if it's not bot's turn
-    if (turn != botPiece) {
-      console.log("it is not bot's turn");
-      return;
-    } else {
-      console.log("it is bot's turn");
-    }
-
-    if (botRemaining > 0) {
-      let [newIndex, oldIndex] = bot.play(board, false);
-        updateBoard(newIndex);
-        changeTurn();
-    } else {
-      let [newIndex, oldIndex] = bot.play(board, true);
-        updateBoard(newIndex, oldIndex);
-        changeTurn();
-    }
-    console.log("board:", board);
-  };
-
-  const changeTurn = () => {
-    setTurn(turn === 1 ? 2 : 1);
   };
 
   return (
     <View style={styles.container}>
       {modalView()}
-      <Text style={[styles.title]}>
-        Player is {playerPiece === 1 ? "black" : "white"}
+      <Text style={styles.title}>
+        Player goes first.
       </Text>
       <Board
         board={board}
         handlePress={(index) => {
           handlePress(index);
-            letBotPlay();
         }}
         pressedIndex={pressedIndex}
       />
       <TouchableOpacity
         onPress={() => {
-                  resetGame();
-                new Promise( resolve => setTimeout(resolve, 1000) );
-          letBotPlay();
+          resetGame();
         }}
         style={styles.button}
       >
@@ -307,6 +222,7 @@ const styles = StyleSheet.create({
     fontSize: 32,
     color: colorPalette.primary1,
     fontWeight: "bold",
+    textAlign: "center",
   },
   container: {
     flex: 1,
--- three_mens_morris/frontend/components/Bot.tsx
+++ three_mens_morris/frontend/components/Bot.tsx
@@ -1,16 +1,6 @@
 import { isValidMove } from "./Rules";
 
 export default class Bot {
-    piece: number;
-    
-  constructor(piece: number) {
-    this.piece = piece;
-  }
-    
-    changePiece(piece: number) {
-        this.piece = piece;
-    }
-    
   getRandIndex(board: number[]): number {
     let unoccupied_positions = [];
 
@@ -27,6 +17,27 @@ export default class Bot {
     ];
   }
 
+  getPotentialWin(board: number[], piece: number): number {
+  // return the index if placed there it will connect 2 pieces
+  let potentialIndices: number[] = [];
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === 0) {
+      let newBoard = board.slice();
+      newBoard[i] = piece;
+      let [winnable, ,] = this.checkWinningIndex(newBoard, piece);
+      if (winnable) {
+        potentialIndices.push(i);
+      }
+    }
+  }
+    
+  if (potentialIndices.length === 0) {
+    return -1;
+  }
+    
+  return potentialIndices[Math.floor(Math.random() * potentialIndices.length)];
+}
+
   getRandMove(board: number[]): [number, number] {
     // return new index and old index that are valid and random.
     let unoccupied_positions: number[] = [];
@@ -38,31 +49,34 @@ export default class Bot {
       if (board[i] === 0) {
         unoccupied_positions.push(i);
       }
-      if (board[i] === this.piece) {
+      if (board[i] === 2) {
         occupied_positions.push(i);
       }
     }
 
     for (let oldIndex of occupied_positions) {
-      for (let newIndex of unoccupied_positions) {
-        if (isValidMove(oldIndex, newIndex)) {
-          validMoves.push([newIndex, oldIndex]);
+      for (let newIndexRand of unoccupied_positions) {
+        if (isValidMove(oldIndex, newIndexRand)) {
+          validMoves.push([newIndexRand, oldIndex]);
         }
       }
     }
 
     if (validMoves.length === 0) {
-      return [-1, -1]; // Return a default value if no valid moves are found
+      return [-1, -1]; // Bot loses
     }
 
     let randomMove: number[] =
       validMoves[Math.floor(Math.random() * validMoves.length)];
     let randomNewIndex: number = randomMove[0];
     let randomOldIndex: number = randomMove[1];
-    return [randomNewIndex, randomOldIndex];
+    return [randomOldIndex, randomNewIndex];
   }
 
-  checkWinningIndex(board: number[], piece?: number): [boolean, number?] {
+  checkWinningIndex(
+    board: number[],
+    piece: number
+  ): [boolean, number?, number[]?] {
     // All possible winning combinations
     const lines = [
       [0, 1, 2],
@@ -79,51 +93,153 @@ export default class Bot {
 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-      if (
-        board[a] === this.piece &&
-        board[b] === this.piece &&
-        board[c] === 0
-      ) {
+      if (board[a] === piece && board[b] === piece && board[c] === 0) {
         winningIndex.push(c);
       }
-      if (
-        board[a] === this.piece &&
-        board[c] === this.piece &&
-        board[b] === 0
-      ) {
+      if (board[a] === piece && board[c] === piece && board[b] === 0) {
         winningIndex.push(b);
       }
-      if (
-        board[b] === this.piece &&
-        board[c] === this.piece &&
-        board[a] === 0
-      ) {
+      if (board[b] === piece && board[c] === piece && board[a] === 0) {
         winningIndex.push(a);
       }
     }
 
     if (winningIndex.length === 0) {
       return [false];
-    } else {
-      // return a random winning index
-      return [
-        true,
-        winningIndex[Math.floor(Math.random() * winningIndex.length)],
-      ];
     }
+
+    // return a random winning index
+    let randomIndex = Math.floor(Math.random() * winningIndex.length);
+    return [true, winningIndex[randomIndex]];
   }
 
-  checkOpsWinningIndex(board: number[]): [boolean, number?] {
-    let opponentPiece = this.piece === 1 ? 2 : 1;
-    return this.checkWinningIndex(board, opponentPiece);
+  getWinningMove(board: number[], piece: number): [boolean, number?, number?] {
+    // get the move that will win the game
+    // return old move, new move if winnable else false
+    console.log("checking wining move for piece:", piece);
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // get all positions by the piece on board
+    let indices = [];
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === piece) {
+        indices.push(i);
+      }
+    }
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      let lockedIndices = [];
+      let remainingIndex = [...indices];
+
+      // check if two out of three positions make a check
+      // and get the third position.
+      if (indices.includes(a)) {
+        lockedIndices.push(a);
+        remainingIndex = remainingIndex.filter(index => index !== a);
+      }
+      if (indices.includes(b)) {
+        lockedIndices.push(b);
+        remainingIndex = remainingIndex.filter(index => index !== b);
+      }
+      if (indices.includes(c)) {
+        lockedIndices.push(c);
+        remainingIndex = remainingIndex.filter(index => index !== c);
+      }
+      console.log("match:", lockedIndices.length, "index needs moving", remainingIndex[0]);
+      // see if missing index can be a valid move
+      if (lockedIndices.length === 2 && remainingIndex.length === 1) {
+        if (board[a] === piece && board[b] === piece && board[c] === 0 && this.checkIndexMovePossible(board, piece, remainingIndex[0], c)
+        ) {
+          console.log("winning line:", lines[i], "oldIndex:", remainingIndex[0], "newIndex:", c);
+          return [true, remainingIndex[0], c];
+        }
+        if (board[a] === piece && board[c] === piece && board[b] === 0 && this.checkIndexMovePossible(board, piece, remainingIndex[0], b)) {
+          console.log("winning line:", lines[i], "oldIndex:", remainingIndex[0], "newIndex:", b);
+          return [true, remainingIndex[0], b];
+        }
+        if (board[b] === piece && board[c] === piece && board[a] === 0 && this.checkIndexMovePossible(board, piece, remainingIndex[0], a)) {
+          console.log("winning line:", lines[i], "oldIndex:", remainingIndex[0], "newIndex:", a);
+          return [true, remainingIndex[0], b];
+        }
+      }
+    }
+
+    return [false];
   }
 
-  checkMovePossible(board: number[], targetIndex: number): [boolean, number?] {
+  getRandomMoveToIndex(board: number[], piece: number, targetIndex: number): number {
+    // get the old index to move to target index
+    let indices = [];
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === piece && isValidMove(i, targetIndex)) {
+        console.log("player", piece, i, "to", targetIndex, "valid")
+        indices.push(i);
+      }
+    }
+
+    if (indices.length === 0) {
+      return -1;
+    }
+
+    return indices[Math.floor(Math.random() * indices.length)];
+  }
+
+  checkIndexMovePossible(
+    board: number[],
+    piece: number,
+    oldIndex: number,
+    targetIndex: number
+  ): boolean {
+    // can't move if it ain't yours
+    if (board[oldIndex] != piece) {
+      console.log("player", piece, oldIndex, "to", targetIndex, "invalid");
+      return false;
+    }
+
+    // can't move to an occupied position
+    if (board[targetIndex] != 0) {
+      console.log("player", piece, oldIndex, "to", targetIndex, "invalid");
+      return false;
+    }
+
+    if (isValidMove(oldIndex, targetIndex)) {
+      console.log("player", piece, oldIndex, "to", targetIndex, "valid");
+      return true;
+    }
+    
+    console.log("player", piece, oldIndex, "to", targetIndex, "invalid");
+    return false;
+  }
+  
+  checkMovePossible(
+    board: number[],
+    piece: number,
+    targetIndex: number,
+  ): [boolean, number?] {
+    // check if target index is reachable from any old index
+    // if true return a random old index
+    // do not try to move the piece that is in setupIndices
+
+    // do not move to target index if it is already occupied
+    if (board[targetIndex] != 0) {
+      return [false];
+    }
+
     let oldIndices = [];
     for (let i = 0; i < board.length; i++) {
-      if (board[i] === this.piece && isValidMove(i, targetIndex)) {
-        // TODO: eliminate move that will expose to lost game.
-        oldIndices.push(i);
+      if (board[i] === piece && isValidMove(i, targetIndex)) {
+          console.log("player", piece, i, "to", targetIndex, "valid");
+          oldIndices.push(i);
       }
     }
 
@@ -135,58 +251,106 @@ export default class Bot {
     return [true, oldIndex];
   }
 
-  play(board: number[], move: boolean): [number, number?] {
-    // new index
-    // optional old index
-    let [winnable, index] = this.checkWinningIndex(board, this.piece);
-
-    // bot is moving a piece
-    if (move) {
-      if (winnable) {
-        // check if any move is possible to win
-        let [winMovePossible, oldIndex] = this.checkMovePossible(
-          board,
-          index as number
-        );
-        if (winMovePossible) {
-          return [index as number, oldIndex as number];
-        } else {
-          let [opponentWinnable, opponentIndex] =
-            this.checkOpsWinningIndex(board);
-          if (opponentWinnable) {
-            let [blockMovePossible, oldIndex] = this.checkMovePossible(
-              board,
-              opponentIndex as number
-            );
-            if (blockMovePossible) {
-              return [opponentIndex as number, oldIndex as number];
-            } else {
-              // GG
-              return this.getRandMove(board);
-            }
-          }
+  getStaleIndices(board: number[], piece: number): number[] {
+    // check the board and return all indices that will make the opponent win
+    let opponent = piece === 1 ? 2 : 1;
+    let staleIndices = [];
+    // replace all pieces of the player
+    for (let i = 0; i < 9; i++) {
+      let tryBoard = [...board];
+      if (tryBoard[i] === piece) {
+        tryBoard[i] = 0;
+        if (this.checkWinningIndex(tryBoard, opponent)[0] && this.checkMovePossible(tryBoard, opponent, i)[0]) {
+          staleIndices.push(i);
         }
-      } else {
+      };
+    }
+    return staleIndices;
+  }
+
+  play_move(board: number[]): [number, number] {
+    // [old index, new index]
+    let [winnable, index] = this.checkWinningIndex(board, 2);
+    console.log("winnable:", winnable, "index:", index);
+    if (winnable) {
+      // check if any move is possible to win
+      let [winMovePossible, oldIndex, newIndex] = this.getWinningMove(board, 2);
+      if (winMovePossible) {
+        return [oldIndex as number, newIndex as number];
+      }
+      console.log("winnable: ", winMovePossible);
+
+      // check if bot needs to block opponent
+      let [opponentWinnable, opponentOldIndex, opponentNewIndex] = this.getWinningMove(
+        board,
+        1
+      );
+      
+      // if there is no move to block then make a random move
+      if (!opponentWinnable) {
         return this.getRandMove(board);
       }
-    } else {
-      // bot is placing a new piece
-      if (winnable) {
-        return [index as number];
-      } else {
-        // block opposite winning index
-        let [opponentWinnable, opponentIndex] =
-          this.checkOpsWinningIndex(board);
-        if (opponentWinnable) {
-          // blockable
-          return [opponentIndex as number];
-        } else {
-          // GG
-          return [this.getRandIndex(board)];
-        }
-      }
+
+      // block opponent
+      let oldIndexToBlock = this.getRandomMoveToIndex(board, 2, opponentNewIndex as number);
+      return [oldIndexToBlock, opponentNewIndex as number];
     }
-    // Add a default return statement at the end
-    return [-1];
+    
+    // don't move piece if it makes opponent win
+    // check if bot needs to block opponent
+    let [opponentWinnable, opponentOldIndex, opponentNewIndex] = this.getWinningMove(
+      board,
+      1
+    );
+    
+    // if there is no move to block then make a random move
+    if (!opponentWinnable) {
+      return this.getRandMove(board);
+    }
+
+    // block opponent
+    let oldIndexToBlock = this.getRandomMoveToIndex(board, 2, opponentNewIndex as number);
+    console.log("bot should block:", opponentNewIndex);
+    console.log("bot thinks about moving:", oldIndexToBlock);
+    if (oldIndexToBlock === -1) {
+      return this.getRandMove(board);
+    }
+    return [oldIndexToBlock, opponentNewIndex as number];
+  }
+
+  play_new(board: number[], playerOneRemaining: number): number {
+    // bot is placing a new piece
+    let [winnable, index,] = this.checkWinningIndex(board, 2);
+
+    if (winnable) {
+      return index as number;
+    }
+
+    // block opposite winning index but don't block if opponent can't move there
+    let [opponentWinnable, opponentIndex] = this.checkWinningIndex(board, 1);
+
+    // block at all times if opponent still has new piece to place
+    if (opponentWinnable && playerOneRemaining >= 0) {
+      return opponentIndex as number;
+    }
+
+    if (opponentWinnable && playerOneRemaining === 0) {
+      // if opponent can't move there next move then make a random move
+      let [opponentPossibleWinNext, opponentPossibleWinIndex] = this.checkMovePossible(
+        board,
+        1,
+        opponentIndex as number
+      );
+      if (opponentPossibleWinNext) {
+        return opponentPossibleWinIndex as number;
+      };
+    }
+
+    // make cohesive move
+    let cohesiveIndex = this.getPotentialWin(board, 2);
+    if (cohesiveIndex === -1) { 
+      return this.getRandIndex(board);
+    }
+    return cohesiveIndex;
   }
 }
