6922e546766317bbece1b2228b0bf8919f594ca0
minsung.cho <ms98.cho@gmail.com>
2024-07-16 18:31:58 -0700

    different approach

--- three_mens_morris_v2/simulator/bot.py
+++ three_mens_morris_v2/simulator/bot.py
@@ -1,52 +1,15 @@
 from rules import legal_move_tree, win_lines
 import random
+import copy
 
 class Bot():
     def __init__(self, order: int):
         # 0 or 1
         self.order = order
-
-    def add_new_piece(self, board: list[list[int]], index: int) -> list[list[int]]:
-        new_board = board.copy()
-        new_board[self.order].append(index)
-        # sort the new board
-        new_board[self.order].sort()
-        return new_board
     
-    def update_board(self, board: list[list[int]], old_move: int, new_move: int) -> list[list[int]]:
-        new_board = board.copy()
-        new_board[self.order].remove(old_move)
-        new_board[self.order].append(new_move)
-        # sort the new board
-        new_board[self.order].sort()
-        return new_board
-    
-    def get_early_win(self, board: list[list[int]]):
-        possible_index = []
-        for line in win_lines:
-            temp_line = line.copy()
-            for index in board[self.order]:
-                if index in temp_line:
-                    temp_line.remove(index)
-            
-            if len(temp_line) != 1:
-                return None
-            
-            enemy_order = 1 if self.order == 0 else 0
-            for index in board[enemy_order]:
-                if index == temp_line[0]:
-                    return None
-            
-            possible_index.append(temp_line[0])
-
-        if len(possible_index) == 0:
-            return None
-        
-        new_index = random.choice(possible_index)
-        return self.add_new_piece(board, new_index)
-
-    def get_winning_index(self, board: list[list[int]]):
-        possible_moves = []
+    def get_all_winning_moves(self, board: list[list[int]]) -> list[tuple[int, int]]:
+        # takes the board and returns all the possible winning moves in tuple
+        possible_moves: list[tuple[int, int]] = []
         for line in win_lines:
             temp_line = line.copy()
             movable_index = board[self.order].copy()
@@ -56,40 +19,72 @@ class Bot():
                     movable_index.remove(index)
             
             if len(temp_line) != 1:
-                return None
+                continue
+
+            new_index = temp_line[0]
+            old_index = movable_index[0]
             
+            # check if new index is already occupied
             enemy_order = 1 if self.order == 0 else 0
-            for index in board[enemy_order]:
-                if index == temp_line[0]:
-                    return None
+            if new_index in board[enemy_order]:
+                continue
             
-            # check if remaining index can be moved there
-            if temp_line[0] in legal_move_tree[str(movable_index[0])]:
-                possible_moves.append((index, temp_line[0]))
+            # check if new index can be moved from old index
+            if new_index in legal_move_tree[str(old_index)]:
+                possible_moves.append((old_index, new_index))
 
         if len(possible_moves) == 0:
-            return None
-        
-        old_move, new_move = random.choice(possible_moves)
-        return self.update_board(board, old_move, new_move)
+            return []
     
-    def make_move(self, board: list[list[int]]) -> list[list[int]]:
-        # get a move
-        if len(board[self.order]) < 3:
-            win_index = self.get_early_win(board)
-            if win_index is not None:
-                return win_index
-            return self.make_random_piece(board)
-        else:
-            new_board = self.get_winning_index(board)
-            if new_board is not None:
-                return new_board
-            else:
-                return self.make_random_move(board)
+        return possible_moves
+    
+    def get_all_winning_new_piece(self, board: list[list[int]]) -> list[int]:
+        # takes the board and returns all the possible winning new piece
+        possible_index: list[int] = []
+        for line in win_lines:
+            temp_line = line.copy()
+            for index in board[self.order]:
+                if index in temp_line:
+                    temp_line.remove(index)
+            
+            if len(temp_line) != 1:
+                continue
+            
+            new_index = temp_line[0]
 
-    def make_random_move(self, board: list[list[int]]) -> list[list[int]]:
-        # get all possible moves
-        possible_moves: list[tuple[int, int]] = []
+            enemy_order = 1 if self.order == 0 else 0
+            if new_index in board[enemy_order]:
+                continue
+            
+            possible_index.append(new_index)
+
+        if len(possible_index) == 0:
+            return []
+        
+        return possible_index
+
+    def add_new_piece(self, board: list[list[int]], index: int) -> list[list[int]]:
+        new_board = copy.deepcopy(board)
+        new_board[self.order].append(index)
+        # sort the new board
+        new_board[self.order].sort()
+        return new_board
+    
+    def get_all_possible_new_pieces(self, board: list[list[int]]) -> list[int]:
+        possible_index: list[int] = self.get_all_winning_new_piece(board)
+        if len(possible_index) > 0:
+            return possible_index
+        
+        opponent_turn = 1 if self.order == 0 else 0
+        possible_index: list[int] = [i for i in range(9) if i not in board[self.order] and i not in board[opponent_turn]]
+        return possible_index
+    
+    def get_all_possible_moves(self, board: list[list[int]]) -> list[tuple[int, int]]:
+        possible_moves: list[tuple[int, int]] = self.get_all_winning_moves(board)
+        # if winning move exists then only return those
+        if len(possible_moves) > 0:
+            return possible_moves
+        
         # get enemy position
         enemy = 1 if self.order == 0 else 0
         enemy_positions: list[int] = board[enemy]
@@ -98,13 +93,53 @@ class Bot():
             for new_index in legal_move_tree[str(old_index)]:
                 if new_index not in enemy_positions and new_index not in board[self.order]:
                     possible_moves.append((old_index, new_index))
+        return possible_moves
+    
+    def update_board(self, board: list[list[int]], old_move: int, new_move: int) -> list[list[int]]:
+        new_board = copy.deepcopy(board)
+        new_board[self.order].remove(old_move)
+        new_board[self.order].append(new_move)
+        # sort the new board
+        new_board[self.order].sort()
+        return new_board
+    
+    def make_move(self, board: list[list[int]]) -> list[list[int]]:
+        # get a move
+        if len(board[self.order]) < 3:
+            return self.make_random_piece(board)
+        else:
+            return self.make_random_move(board)
         
+    def get_all_moves(self, board: list[list[int]]) -> list[list[list[int]]]:
+        # get all possible moves
+        possible_boards: list[list[list[int]]] = []
+        if len(board[self.order]) < 3:
+            possible_new: list[int] = self.get_all_possible_new_pieces(board)
+            if len(possible_new) == 0:
+                return []
+            else:
+                possible_boards: list[list[list[int]]] = []
+                for move in possible_new:
+                    possible_boards.append(self.add_new_piece(board, move))
+        else:
+            possible_moves: list[tuple[int, int]] = self.get_all_possible_moves(board)
+            if len(possible_moves) == 0:
+                return []
+            for move in possible_moves:
+                possible_boards.append(self.update_board(board, move[0], move[1]))
+        return possible_boards
+
+    def make_random_move(self, board: list[list[int]]) -> list[list[int]]:
+        # get all possible moves
+        possible_moves: list[tuple[int, int]] = self.get_all_possible_moves(board)
+        if len(possible_moves) == 0:
+            return []
         random_move = random.choice(possible_moves)
         return self.update_board(board, random_move[0], random_move[1])
     
     def make_random_piece(self, board: list[list[int]]) -> list[list[int]]:
         # get a random move
-        possible_index = [i for i in range(9) if i not in board[0] and i not in board[1]]
+        possible_index = self.get_all_possible_new_pieces(board)
         random_piece = random.choice(possible_index)
         return self.add_new_piece(board, random_piece)
         
--- three_mens_morris_v2/simulator/main.py
+++ three_mens_morris_v2/simulator/main.py
@@ -1,50 +1,66 @@
 import copy
 import json
-import queue
 from rules import check_win
 from bot import Bot
-
-# initialize the game data
-board: list[list[int]] = [[], []]
-state = 0
-game_over: bool = False
-winner = None
 
 botZero = Bot(0)
 botOne = Bot(1)
 
+def board_to_key(board: list[list[int]]) -> str:
+    # examples:
+    # [[], []] -> "_"
+    # [[0], []] -> "0_"
+    # [[0, 1], [2]] -> "01_2"
+    # [[0, 1], [2, 3]] -> "01_23"
+    player_1 = "".join([str(i) for i in board[0]])
+    player_2 = "".join([str(i) for i in board[1]])
+    return f"{player_1}_{player_2}"
+
 class Node:
-    def __init__(self, layer: int = 0, board: list[list[int]] = [[], []]):
+    def __init__(
+            self,
+            layer: int = 0,
+            board: str = "_",
+            game_over: bool = False,
+            wins: int = 0,
+            visits: int = 0,
+            children: list[str] = []
+            ):
         self.layer = layer
-        self.board = board
-        self.wins = 0
-        self.visits = 0
-        self.game_over = False
-        self.children: list['Node'] = []
+        self.board = self.key_to_board(board)
+        self.wins = wins
+        self.visits = visits
+        self.game_over = game_over
+        self.children: list[str] = children
+        # self.print()
+    
+    def key_to_board(self, key: str) -> list[list[int]]:
+        # examples:
+        # "_" -> [[], []]
+        # "0_1" -> [[0], []]
+        # "01_2" -> [[0, 1], [2]]
+        # "01_23" -> [[0, 1], [2, 3]]
+
+        # split the key into two parts
+        player_1, player_2 = key.split("_")
+        player_1 = [int(i) for i in player_1]
+        player_2 = [int(i) for i in player_2]
+        return [player_1, player_2]
     
     def print(self):
         print(f"{self.layer} {self.board} {self.game_over} {self.wins}")
+        if len(self.children) == 0:
+            return
         for child in self.children:
-            if child is not None:
-                child.print()
-
-    def from_dict(self, data: dict):
-        self.layer = data["layer"]
-        self.board = data["board"]
-        self.game_over = data["game_over"]
-        self.wins = data.get("wins", 0)
-        self.visits = data.get("visits", 0)
-        self.children = [Node().from_dict(child) for child in data.get("children", [])]
-        return self
+            print(child)
     
     def to_dict(self):
         return {
             "layer": self.layer,
-            "board": self.board,
             "game_over": self.game_over,
             "wins": self.wins,
             "visits": self.visits,
-            "children": [child.to_dict() for child in self.children]
+            "children": self.children
         }
 
 def render_board(board: list[list[int]]):
@@ -57,95 +73,193 @@ def render_board(board: list[list[int]]):
     # print three rows
     for i in range(3):
         print(flat_board[i * 3], flat_board[i * 3 + 1], flat_board[i * 3 + 2])
+    
+def get_nodes_from_file(file: str) -> list[Node]:
+    with open(file, "r") as f:
+        data = json.load(f)
 
-with open("weight1.json", "r") as f:
-    weight = json.load(f)
-    weight = Node().from_dict(weight)
-    weight.print()
+    # convert key to board
+    node_list: list[Node] = []
+    for key, val in data.items():
+        node_list.append(Node(
+            layer = val["layer"],
+            board = key,
+            game_over = val["game_over"],
+            wins = val["wins"],
+            visits = val["visits"],
+            children = val["children"]
+            ))
+    return node_list
 
-layer = 1
-init_node = Node(0, copy.deepcopy(board))
-current_node = init_node
-while game_over == False:
-    if state == 0:
-        move = botZero.make_move(board) # this updates the board
-        new_node = Node(layer, copy.deepcopy(board))
-        if not check_win(board, 0):
-            state = 1
-            layer += 1
-            new_node.print()
+def find_board_and_layer(node_list: list[Node], board: list[list[int]], layer: int):
+    for node in node_list:
+        if node.layer == layer and node.board == board:
+            return node
+
+def expand(node_list: list[Node]):
+    new = 0
+    for node in node_list:
+        if node.layer >= 6:
+            continue
+        if node.layer % 2 == 0:
+            new_boards = botZero.get_all_moves(node.board)
+            for new_board in new_boards:
+                if new_board not in [node.board for node in node_list]:
+                    new_node = Node(node.layer + 1, board_to_key(new_board))
+                    new_next_boards = botOne.get_all_moves(new_node.board)
+                    for new_next_board in new_next_boards:
+                        new_node.children.append(board_to_key(new_next_board))
+                    if check_win(new_board, 0):
+                        new_node.game_over = True
+                    node_list.append(new_node)
+                    new += 1
+                    continue
+        else:
+            new_boards = botOne.get_all_moves(node.board)
+            for new_board in new_boards:
+                if new_board not in [node.board for node in node_list]:
+                    new_node = Node(node.layer + 1, board_to_key(new_board))
+                    new_next_boards = botZero.get_all_moves(new_node.board)
+                    for new_next_board in new_next_boards:
+                        new_node.children.append(board_to_key(new_next_board))
+                    if check_win(new_board, 1):
+                        new_node.game_over = True
+                    node_list.append(new_node)
+                    new += 1
+                    continue
+    print("new nodes", new)
+
+nodes = get_nodes_from_file("empty_weight.json")
+
+expand(nodes)
+
+new_dict = {}
+for node in nodes:
+    new_dict[board_to_key(node.board)] = node.to_dict()
+
+with open("expanded.json", "w") as f:
+    json.dump(new_dict, f)
+
+exit()
+
+def play_game(base_weight: Node):
+    # initialize the game data
+    # to update win for layer and board match
+    move_history = []
+    state = 0
+    game_over: bool = False
+    winner = None
+    layer = 1
+    current_node = base_weight
+    while game_over == False:
+        board = current_node.board
+        if state == 0:
+            new_board = botZero.make_move(board)
+            # check if the new board is already in the children
+            while new_board not in [child.board for child in current_node.children]:
+                new_board = botZero.make_move(board)
+                new_node = Node(layer, new_board)
+            if not check_win(new_board, 0):
+                state = 1
+                layer += 1
+                # new_node.print()
+                current_node.children.append(new_node)
+                current_node = new_node
+                continue
+            new_node.game_over = True
+            # new_node.print()
+            winner = 0
             current_node.children.append(new_node)
             current_node = new_node
-            continue
-        new_node.game_over = True
-        new_node.print()
-        winner = 0
-        current_node.children.append(new_node)
-        current_node = new_node
-        game_over = True
-    else:
-        board = botOne.make_move(board)
-        new_node = Node(layer, copy.deepcopy(board))
-        if not check_win(board, 1):
-            state = 0
-            layer += 1
-            new_node.print()
+            game_over = True
+        else:
+            board = botOne.make_move(board)
+            new_node = Node(layer, copy.deepcopy(board))
+            if not check_win(board, 1):
+                state = 0
+                layer += 1
+                # new_node.print()
+                current_node.children.append(new_node)
+                current_node = new_node
+                continue
+            new_node.game_over = True
+            # new_node.print()
+            winner = 1
             current_node.children.append(new_node)
             current_node = new_node
-            continue
-        new_node.game_over = True
-        new_node.print()
-        winner = 1
-        current_node.children.append(new_node)
-        current_node = new_node
-        game_over = True
+            game_over = True
 
-# winner is current state
-# bot zero won
-print("winner:", winner)
-print("layer:", layer)
+    # winner is current state
+    # print("winner:", winner)
+    # print("layer:", layer)
 
-# with open("weight1.json", "w") as f:
-#     json.dump(init_tree.to_dict(), f)
+    backpropagate(init_node, winner)
 
-# won_nodes = []
+    return init_node, layer
+
+
 # iterate through it and update wins and visits
 def backpropagate(node: Node, winner: int):
+    node.visits += 1
     if (winner == 0 and node.layer % 2 == 1) or (winner == 1 and node.layer % 2 == 0):
             node.wins += 1
     for child in node.children:
         backpropagate(child, winner)
 
-backpropagate(init_node, winner)
 
-init_node.print()
-
-current_branch = weight
-current_node = init_node
-while current_node.layer != []:
-    print(f"current branch: {current_branch.layer} {current_branch.board}")
-    print(f"current node: {current_node.layer} {current_node.board}")
-    if current_branch.layer == current_node.layer and current_branch.board == current_node.board and current_branch.children != []:
-        print("same layer same board. moving to next layer")
-        current_branch.visits += current_node.visits
-        current_branch.wins += current_node.wins
-        current_node = current_node.children[0]
-        continue
-    if current_branch.layer == current_node.layer - 1 and current_branch.children == []:
-        print("adding node to branch")
-        current_branch.children.append(current_node)
-        break
-    if current_branch.layer == current_node.layer - 1:
-        print(f"current branch children: {len(current_branch.children)}")
-        # check if current node board is in the children
-        for child in current_branch.children:
-            if child.board == current_node.board:
-                current_branch = child
+def update_weight(base_weight: Node, new_game: Node):
+    hit = False
+    current_branch = base_weight
+    current_node = new_game
+    while 1:
+        # print(f"current branch: {current_branch.layer} {current_branch.board}")
+        # print(f"current node: {current_node.layer} {current_node.board}")
+        if current_branch.layer == current_node.layer and current_branch.board == current_node.board:
+            # print("current node children count", len(current_node.children))
+            # print("same layer same board. moving to next layer")
+            current_branch.visits += current_node.visits
+            current_branch.wins += current_node.wins
+            # current is only one line
+            # print(len(current_node.children))
+            if len(current_node.children) == 0:
+                return False
+            current_node = current_node.children[0]
+            continue
+        if current_branch.layer == current_node.layer - 1:
+            # print("checking within children")
+            if current_branch.children == []:
+                current_branch.children.append(current_node)
+                return True
+            # check if current node board is in the children
+            if current_node.board in [child.board for child in current_branch.children]:
+                # print("found child")
+                for child in current_branch.children:
+                    if child.board == current_node.board:
+                        current_branch = child
+                        break
                 continue
-        current_branch.children.append(current_node)
-        break
+            # print("adding node to branch")
+            current_branch.children.append(current_node)
+            return True
+    return hit
 
-weight.print()
+with open("empty_weight.json", "r") as f:
+    weight = json.load(f)
+    weight = Node().from_dict(weight)
 
-with open("weight.json", "w") as f:
-    json.dump(weight.to_dict(), f)
+# while 1:
+#     hit_count = 0
+#     for i in range(100):
+#         # print("game", i)
+#         new_game, layer = play_game()
+#         if layer > 20:
+#             continue
+#         if update_weight(weight, new_game):
+#             hit_count += 1
+#         # weight.print()
+#     if hit_count < 10:
+#         break
+#     print("hit count", hit_count, "out of 100")
+
+# with open("weight.json", "w") as f:
+#     json.dump(weight.to_dict(), f)
